ef8997b35f0236058e7bb96d8f0bfc657f97ddf0
Aryan Zihan Khan <aryanzihanalreadytaken@gmail.com>
2024-05-12 01:56:26 +0600

    Add files via upload

---
+++ BasicVariableUsage.java
@@ -0,0 +1,17 @@
+// this is a program demonstrating the basic usage of the different variable types in Java
+
+public class BasicVariableUsage {
+  public static void main(String[] args) {
+   String name;
+   int age;
+   char bloodgroup;
+   
+   name = "Aryan Zihan Khan";
+   age = 18;
+   bloodgroup = 'O';
+   
+   System.out.println("Name : "+name);
+   System.out.println("Age : "+age+" years");
+   System.out.print("Blood group : "+bloodgroup+" positive");
+}
+}
