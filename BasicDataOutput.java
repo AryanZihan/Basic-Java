549efad627627ca68c0426f98d753a6f38d97eba
Aryan Zihan Khan <aryanzihanalreadytaken@gmail.com>
2024-05-13 01:34:35 +0600

    Update BasicDataOutput.java

--- BasicDataOutput.java
+++ BasicDataOutput.java
@@ -1,9 +1,9 @@
 // this is a program demonstrating the basic method of data output in Java
 
-public class BasicDataOutput{
-  public static void main(String[] args){
+public class BasicDataOutput {
+  public static void main(String[] args) {
   
     System.out.println("Java test program");
     System.out.print("Test version : 1.0");
-}                       
+ }                       
 }
