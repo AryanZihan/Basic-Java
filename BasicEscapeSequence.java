f651fe31a2faea4e09abfa2f0f34d9886f756b04
Aryan Zihan Khan <aryanzihanalreadytaken@gmail.com>
2024-05-13 00:24:01 +0600

    Add files via upload

---
+++ BasicEscapeSequence.java
@@ -0,0 +1,11 @@
+public class BasicEscapeSequence {
+  public static void main(String[] args) {
+    
+    System.out.println("\"Test Program\"");
+    System.out.print("Test version\t 1.0 \n");
+    System.out.println("Creation date \r");
+    System.out.println("13/05/2024");
+  }
+}
+                   
+    
