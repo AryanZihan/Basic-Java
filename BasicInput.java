be1e216e8b5721116ba9e6ff1c2708ac82f2b29c
Aryan Zihan Khan <aryanzihanalreadytaken@gmail.com>
2024-05-14 00:12:18 +0600

    Add files via upload

---
+++ BasicInput.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+  
+  public class BasicInput {
+   public static void main(String[] args) {
+    
+     Scanner input = new Scanner(System.in);
+     String name;
+     
+     System.out.println("Enter your name : ");
+     name = input.nextLine();
+     
+     System.out.print("Your name has been registered as "+name);
+   }
+}  
