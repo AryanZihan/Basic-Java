969ecea56ba7d366cf97415e65b728b1ff89f3b7
Aryan Zihan Khan <aryanzihanalreadytaken@gmail.com>
2024-05-15 01:13:15 +0600

    Add files via upload

---
+++ BasicFormatSpecifier.java
@@ -0,0 +1,17 @@
+public class BasicFormatSpecifier {
+  public static void main(String[] args) {
+     String name;
+   int age;
+   char bloodgroup;
+   
+   name = "Aryan Zihan Khan";
+   age = 18;
+   bloodgroup = 'O';
+   
+   System.out.printf("Name : %s\n",name);
+   System.out.printf("Age : %d",age);
+   System.out.print(" years\n");                 
+   System.out.printf("Blood group : %c",bloodgroup);
+   System.out.print(" positive");                  
+  }
+}
