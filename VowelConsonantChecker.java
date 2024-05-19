d2d8653015fc868f2e719dd506da435a853b3a67
Aryan Zihan Khan <aryanzihanalreadytaken@gmail.com>
2024-05-19 21:25:53 +0600

    Add files via upload

---
+++ VowelConsonantChecker.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class VowelConsonantChecker {
+  public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+    
+    char letter;
+    System.out.println("Enter any alphabet: ");
+    letter = input.next().charAt(0);
+    
+    if(letter=='a' || letter=='e' || letter=='i' || letter=='o' || letter=='u' || letter=='A' || letter=='E' || letter=='I' || letter=='O' || letter=='U') 
+    {
+      System.out.print("The entered alphabet is a vowel");
+    }
+    
+    else if(letter >='a' && letter <='Z') 
+    {
+      System.out.print("The entered alphabet is a consonant");
+    }
+    
+    else
+    { 
+      System.out.print("Sorry, the entered character is not an alphabet");
+    }
+  }
+}    
