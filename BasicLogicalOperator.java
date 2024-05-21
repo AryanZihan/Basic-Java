beb85375f3a342dd5ee1afdfefe0a0e483bd50e4
Aryan Zihan Khan <aryanzihanalreadytaken@gmail.com>
2024-05-21 18:12:22 +0600

    Add files via upload

---
+++ BasicLogicalOperator.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class BasicLogicalOperator {
+  public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+    int num1, num2;
+    
+    System.out.println("Enter first number: ");
+    num1 = input.nextInt();
+    
+    System.out.println("Enter second number: ");
+    num2 = input.nextInt();
+    
+    if(num1>num2)
+    {
+      System.out.print(num1+"is the greater number");
+    }
+    
+    else
+    {
+      System.out.print(num2+" is the greater number");
+    }
+  }                  
+}                         
+                         
+ 
