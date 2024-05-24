92e45e8374fafeb0bd8cbc13003a121b40fb8984
Aryan Zihan Khan <aryanzihanalreadytaken@gmail.com>
2024-05-25 00:16:00 +0600

    Add files via upload

---
+++ BasicDoWhileLoop.java
@@ -0,0 +1,13 @@
+public class BasicDoWhileLoop {
+  public static void main(String[] args) {
+    int i;
+    i = 1;
+      
+    do
+    {
+      System.out.println("Test");
+      i++;
+    }
+    while(i<=10);
+  }
+}  
