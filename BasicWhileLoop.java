31587aecf6badbbedbb7d6a76f1595336e65d658
Aryan Zihan Khan <aryanzihanalreadytaken@gmail.com>
2024-05-24 21:27:44 +0600

    Update BasicWhileLoop.java

--- BasicWhileLoop.java
+++ BasicWhileLoop.java
@@ -1,3 +1,5 @@
+//this Java program demonstrates the basic use of while loop
+
 public class BasicWhileLoop {
   public static void main(String[] args) {
   int i;
@@ -5,6 +7,7 @@ public class BasicWhileLoop {
   while(i<=10)
   {
     System.out.println("Test");
+    //the following text will be printed 10 times
     i++;
   }
   }
