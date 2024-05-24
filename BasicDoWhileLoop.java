a4e3ec475077816d0fa515e9b10c40adfdc3bb0d
Aryan Zihan Khan <aryanzihanalreadytaken@gmail.com>
2024-05-25 00:18:21 +0600

    Update BasicDoWhileLoop.java

--- BasicDoWhileLoop.java
+++ BasicDoWhileLoop.java
@@ -1,3 +1,5 @@
+//this is a Java program which demonstrates the basic function of do while loop
+
 public class BasicDoWhileLoop {
   public static void main(String[] args) {
     int i;
@@ -6,6 +8,7 @@ public class BasicDoWhileLoop {
     do
     {
       System.out.println("Test");
+      //the following text will be printed 10 times
       i++;
     }
     while(i<=10);
